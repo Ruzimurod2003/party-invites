c94de0db275d3a42e2d0436ca072630f3bc42e12
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2022-09-12 22:28:27 +0500

    initial

---
+++ Models/data.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models {
+    public class data {
+        public int Id{get;set;}
+        public string Ism{get;set;}
+        public string Nomer{get;set;}
+        public string Pochta{get;set;}
+    }
+}
